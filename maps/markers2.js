1f6c92ea9b752d7c49b90eb640212c2e16a3a5b5
Fl0Ch3v <65029165+Fl0Ch3v@users.noreply.github.com>
2020-12-11 16:20:59 +0100

    Update markers2.js

--- maps/markers2.js
+++ maps/markers2.js
@@ -1,15 +1,15 @@
 var markers = [
-    {"name":"Nuit de la chauve-souris","com":"Caours","lat":"1.8812529503","lon":"50.1281700444"},
-{"name":"Les marais de Long","Caours":"Long","1.8812529503":"1.9717017418","50.1281700444":"50.0356781024"},
-{"name":"Des jardins de Folie","Long":"Long","1.9717017418":"1.9825201423","50.0356781024":"50.0382186575"},
-{"name":"Des étoiles pleins les yeux","Long":"Long","1.9825201423":"1.9776182911","50.0382186575":"50.0382560101"},
-{"name":"De l'abeille au pot : à la découverte du miel en direct du producteur","Long":"Long","1.9776182911":"1.9777943994","50.0382560101":"50.0381855953"},
-{"name":"Nuit de la chouette","Long":"Doudelainville","1.9777943994":"1.7640272923","50.0381855953":"50.0010287975"},
-{"name":"Les marais de Saint-Gilles","Doudelainville":"Abbeville","1.7640272923":"1.8341298893","50.0010287975":"50.0899311031"},
-{"name":"La Bouvaque: un îlôt de nature préservée aux portes de la ville","Abbeville":"Abbeville","1.8341298893":"1.8399557575","50.0899311031":"50.1151752301"},
-{"name":"Sur les traces des insectes volants","Abbeville":"Longpré-les-Corps-Saints","1.8399557575":"1.9822829626","50.1151752301":"50.0248525425"},
-{"name":"Les p'tites bêtes de la mare","Longpré-les-Corps-Saints":"Longpré-les-Corps-Saints","1.9822829626":"1.9811031025","50.0248525425":"50.0258709994"},
-{"name":"La richesse vivante des tourbières","Longpré-les-Corps-Saints":"Longpré-les-Corps-Saints","1.9811031025":"1.9848972624","50.0258709994":"50.0258524762"},
-{"name":"A la rencontre des papillons de nuit","Longpré-les-Corps-Saints":"Longpré-les-Corps-Saints","1.9848972624":"1.9850014427","50.0258524762":"50.020874551"},
-{"name":"A la rencontre de la forêt de Crécy","Longpré-les-Corps-Saints":"Crécy-en-Ponthieu","1.9850014427":"1.8451103217","50.020874551":"50.2403872144"}
+    {"name":"Nuit de la chauve-souris","com":"Caours","lat":1.8812529503,"lng":50.1281700444},
+    {"name":"Les marais de Long","Caours":"Long","lat":1.9717017418,"lng":50.0356781024},
+    {"name":"Des jardins de Folie","Long":"Long","lat":1.9825201423,"lng":50.0382186575},
+    {"name":"Des étoiles pleins les yeux","Long":"Long","lat":1.9776182911,"lng":50.0382560101},
+    {"name":"De l'abeille au pot : à la découverte du miel en direct du producteur","Long":"Long","lat":1.9777943994,"lng":50.0381855953},
+    {"name":"Nuit de la chouette","Long":"Doudelainville","lat":1.7640272923,"lng":50.0010287975},
+    {"name":"Les marais de Saint-Gilles","Doudelainville":"Abbeville","lat":1.8341298893,"lng":50.0899311031},
+    {"name":"La Bouvaque: un îlôt de nature préservée aux portes de la ville","Abbeville":"Abbeville","lat":1.8399557575,"lng":50.1151752301},
+    {"name":"Sur les traces des insectes volants","Abbeville":"Longpré-les-Corps-Saints","lat":1.9822829626,"lng":50.0248525425},
+    {"name":"Les p'tites bêtes de la mare","Longpré-les-Corps-Saints":"Longpré-les-Corps-Saints","lat":1.9811031025,"lng":50.0258709994},
+    {"name":"La richesse vivante des tourbières","Longpré-les-Corps-Saints":"Longpré-les-Corps-Saints","lat":1.9848972624,"lng":50.0258524762},
+    {"name":"A la rencontre des papillons de nuit","Longpré-les-Corps-Saints":"Longpré-les-Corps-Saints","lat":1.9850014427,"lng":50.020874551},
+    {"name":"A la rencontre de la forêt de Crécy","Longpré-les-Corps-Saints":"Crécy-en-Ponthieu","lat":1.8451103217,"lng":50.2403872144}
 ];
